af56f52d34b7510adc8efde675138b9c656d1c76
CheSebas <rodulfsebas@hotmail.com>
2025-08-12 20:19:58 -0500

    Primer commit

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Nivel_2-Practica-1'
+  base: '/Nivel_2-Practica-1/'
 })
