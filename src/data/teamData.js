2bc2e45c59ad67ce08d53d83decd1f6b7af923f6
CheSebas <rodulfsebas@hotmail.com>
2025-08-12 19:31:41 -0500

    Primer commit

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -3,36 +3,36 @@ export const teamData = [
     id: 1,
     name: "Bill Mahoney",
     role: "Product Owner",
-    image: "/recursos/images/photo1.png"
+    image: `${import.meta.env.BASE_URL}recursos/images/photo1.png`
   },
   {
     id: 2,
     name: "Saba Cabrera",
     role: "Art Director",
-    image: "/recursos/images/photo2.png"
+    image: `${import.meta.env.BASE_URL}recursos/images/photo2.png`
   },
   {
     id: 3,
     name: "Shae Le",
     role: "Tech Lead",
-    image: "/recursos/images/photo3.png"
+    image: `${import.meta.env.BASE_URL}recursos/images/photo3.png`
   },
   {
     id: 4,
     name: "Skylah Lu",
     role: "UX Designer",
-    image: "/recursos/images/photo4.png"
+    image: `${import.meta.env.BASE_URL}recursos/images/photo4.png`
   },
   {
     id: 5,
     name: "Griff Richards",
     role: "Developer",
-    image: "/recursos/images/photo5.png"
+    image: `${import.meta.env.BASE_URL}recursos/images/photo5.png`
   },
   {
     id: 6,
     name: "Stan John",
     role: "Developer",
-    image: "/recursos/images/photo6.png"
+    image: `${import.meta.env.BASE_URL}recursos/images/photo6.png`
   }
 ];
