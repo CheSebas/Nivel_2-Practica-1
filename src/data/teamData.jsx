95fd64378727f4f484b311ca6e49f3683378e26e
CheSebas <rodulfsebas@hotmail.com>
2025-08-14 18:20:00 -0500

    Deploy

---
+++ src/data/teamData.jsx
@@ -0,0 +1,77 @@
+export const teamData = [
+  {
+    id: 1,
+    name: "Bill Mahoney",
+    role: "Product Owner",
+    image: "recursos/images/photo1.png",
+  },
+  {
+    id: 2,
+    name: "Saba Cabrera",
+    role: "Art Director",
+    image: "recursos/images/photo2.png",
+  },
+  {
+    id: 3,
+    name: "Shae Le",
+    role: "Tech Lead",
+    image: "recursos/images/photo3.png",
+  },
+  {
+    id: 4,
+    name: "Skylah Lu",
+    role: "UX Designer",
+    image: "recursos/images/photo4.png",
+  },
+  {
+    id: 5,
+    name: "Griff Richards",
+    role: "Developer",
+    image: "recursos/images/photo5.png",
+  },
+  {
+    id: 6,
+    name: "Stan John",
+    role: "Developer",
+    image: "recursos/images/photo6.png",
+  },
+];
+
+// export const teamData = [
+//   {
+//     id: 1,
+//     name: "Bill Mahoney",
+//     role: "Product Owner",
+//     image: `${import.meta.env.BASE_URL}recursos/images/photo1.png`
+//   },
+//   {
+//     id: 2,
+//     name: "Saba Cabrera",
+//     role: "Art Director",
+//     image: `${import.meta.env.BASE_URL}recursos/images/photo2.png`
+//   },
+//   {
+//     id: 3,
+//     name: "Shae Le",
+//     role: "Tech Lead",
+//     image: `${import.meta.env.BASE_URL}recursos/images/photo3.png`
+//   },
+//   {
+//     id: 4,
+//     name: "Skylah Lu",
+//     role: "UX Designer",
+//     image: `${import.meta.env.BASE_URL}recursos/images/photo4.png`
+//   },
+//   {
+//     id: 5,
+//     name: "Griff Richards",
+//     role: "Developer",
+//     image: `${import.meta.env.BASE_URL}recursos/images/photo5.png`
+//   },
+//   {
+//     id: 6,
+//     name: "Stan John",
+//     role: "Developer",
+//     image: `${import.meta.env.BASE_URL}recursos/images/photo6.png`
+//   }
+// ];
